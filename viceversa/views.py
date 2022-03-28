fc8c34dc72351738f3bc40f97b193d072d68fe73
Верхотуров Александр Александрович <Verkhoturov.A@partner.ru>
2022-03-28 16:14:20 +1000

    добавил реверс в том же окне

--- viceversa/views.py
+++ viceversa/views.py
@@ -5,8 +5,15 @@ def about(request):
     return HttpResponse('This is about page')
 
 def home(request):
-
-    return render(request, 'home.html')
+   ## text_get = ReversStr(request.GET['TextReverse2'])
+    if str(request).find('TextReverse2') == -1:
+        text_get = ''
+    else:
+        text_get = ReversStr(request.GET['TextReverse2'])        
+    print(request)
+    if text_get == '':
+        text_get = ""
+    return render(request, 'home.html',{'TextReverse3':text_get})
 
 def reverse(request):
     text_get = ReversStr(request.GET['TextReverse'])
